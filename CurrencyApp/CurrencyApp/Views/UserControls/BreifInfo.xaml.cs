f1385e6cd32e7e49418c7b3637f038ce581c30d0
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-05-30 02:12:32 +0300

    UserControls

---
+++ CurrencyApp/CurrencyApp/Views/UserControls/BreifInfo.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurrencyApp.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BreifInfo.xaml
+    /// </summary>
+    public partial class BreifInfo : UserControl, INotifyPropertyChanged
+    {    
+        public string CurrencyName
+        {
+            get 
+            { 
+                return CurrencyName; 
+            }
+            set 
+            {
+                CurrencyName = value; 
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("CurrencyName"));
+            }
+        }
+        public string Sign 
+        { 
+            get 
+            { 
+                return Sign; 
+            } 
+            set 
+            { 
+                Sign = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Sign"));
+
+            }
+        } 
+        public string CurrencyType 
+        {
+            get
+            {
+                return CurrencyType;
+            }
+            set 
+            {
+                CurrencyType = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrencyType"));
+
+            }
+        }
+        public string USDValue
+        {
+            get
+            {
+                return USDValue;
+            }
+            set
+            {
+                USDValue = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("USDValue"));
+
+            }
+        }
+
+        public BreifInfo()
+        {
+            InitializeComponent();
+            DataContext=this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ CurrencyApp/CurrencyApp/Views/UserControls/SearchTextBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CurrencyApp.Views.UserControls
+{
+
+    public partial class SearchTextBox : UserControl
+    {
+        public SearchTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Text { get; set; }
+        private void btnClearText(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceHolder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceHolder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
